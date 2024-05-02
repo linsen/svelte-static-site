959bd9c30eec62bfae8af6813d5014ea8fcdb174
Linsen Loots <linsen@10x.co.za>
2024-05-02 15:28:16 +0200

    feat: minimal setup done for a static site

---
+++ src/lib/components/ComponentBlock.svelte
@@ -0,0 +1,10 @@
+<div>
+  <h2>This is a component</h2>
+  <p>It has its own styles</p>
+</div>
+
+<style>
+  p {
+    color: blue;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import ComponentBlock from "$lib/components/ComponentBlock.svelte";
+  const templateVar = "Hello World!";
+</script>
+
+<h1>{templateVar}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ComponentBlock />
